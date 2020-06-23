e56ac62de1818cd22705148ceb643704db479d18
Jared Hubbard <hubbard.abstract@gmail.com>
2020-06-22 19:08:04 -0700

    commented out not needed code

--- DjangoUni/Classes/classApp/views.py
+++ DjangoUni/Classes/classApp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from Classes import classApp
 from Classes.classApp.models import djangoClasses
 
-class_CompSci = djangoClasses(Title='Computer Science', Course='101', Instructor='Dr.Cash',
-                              Duration='2 Hours')
-class_FrontEnd = djangoClasses(Title='Front End Developer', Course='205', Instructor='Mr.Lowery',
-                               Duration='3 Hours')
-class_BackEnd = djangoClasses(Title='Front End Developer', Course='505', Instructor='Dr.Teeth',
-                              Duration='8 Hours')
+#class_CompSci = djangoClasses(Title='Computer Science', Course='101', Instructor='Dr.Cash',
+ #                             Duration='2')
+#class_FrontEnd = djangoClasses(Title='Front End Developer', Course='205', Instructor='Mr.Lowery',
+  #                             Duration='3')
+#class_BackEnd = djangoClasses(Title='Front End Developer', Course='505', Instructor='Dr.Teeth',
+   #                           Duration='8')
 
-class_list = [class_CompSci, class_FrontEnd, class_BackEnd]
+#class_list = [class_CompSci, class_FrontEnd, class_BackEnd]
 
-djangoClasses.objects.bulk_create(class_list)
+#djangoClasses.objects.bulk_create(class_list)
