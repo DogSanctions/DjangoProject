da263e02d8ce55567a928b1969ab06264d7a0aca
Jared Hubbard <hubbard.abstract@gmail.com>
2020-06-22 17:56:17 -0700

    took out non working projects and added new working one

--- DjangoUniversity/Classes/classApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassAppConfig(AppConfig):
-    name = 'classes'
--- DjangoUniversity/Classes/classApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import djangoClasses
-
-
-class djangoClassesForm(ModelForm):
-    class Meta:
-        model = djangoClasses
-        fields = '__all__'
